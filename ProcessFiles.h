e5cb6418d50414dec7142590b18099f5ac677b08
Theodosis Melidis <melidisteo@Gmail.com>
2017-07-18 16:59:15 +0300

    Update ProcessFiles.h

--- ProcessFiles.h
+++ ProcessFiles.h
@@ -49,90 +49,90 @@ public:
      * getter for private member documentsText
      * @return the ifstream documentsText
      */
-	ifstream& getDocumentsText() { return documentsText; }
+     ifstream& getDocumentsText() { return documentsText; }
     
     /**
      * getter for private member queriesText
      * @return the ifstream queriesText
      */
-	ifstream& getQueriesText() { return queriesText; }
+     ifstream& getQueriesText() { return queriesText; }
     
     /**
      * getter for private member documentsTokens
      * @return the list of documentsTokens
      */
-	list<string>* getDocumentsTokens() const { return documentsTokens; }
+     list<string>* getDocumentsTokens() const { return documentsTokens; }
     
     /**
      * getter for private member queriesTokens
      * @return the list of queriesTokens
      */
-	list<string>* getQueriesTokens() const { return queriesTokens; }
+     list<string>* getQueriesTokens() const { return queriesTokens; }
     
     /**
      * getter for private member nDocuments
      * @return the number of documents
      */
-	size_t getNDocuments() const { return nDocuments; }
+     size_t getNDocuments() const { return nDocuments; }
     
     /**
      * getter for private member nQueries
      * @return the number of queries
      */
-	size_t getNQueries() const { return nQueries; }
+     size_t getNQueries() const { return nQueries; }
     
     /**
      * getter for private member nResponses
      * @return the number of responses
      */
-	map<size_t, size_t> getNResponses() const { return nResponses; }
+     map<size_t, size_t> getNResponses() const { return nResponses; }
     
     /**
      * it reads the documents file. Firstly stores the number of documents
-	 * to variable nDocuments and then documents' tokens to variable 
-	 * documentsTokens according to documents' ids
+     * to variable nDocuments and then documents' tokens to variable 
+     * documentsTokens according to documents' ids
      * @param stream is the stream to be read
      */
     void readDocumentsFile(ifstream& stream);
     
     /**
-	 * it reads the queries file. Firstly stores the number of queries to 
-	 * variable nQueries. Consequently stream gives two numbers to be read
-	 * which stored to map nResponses and finally queries' tokens which are
-	 * stores to variable queriesTokens according to queries' ids
+     * it reads the queries file. Firstly stores the number of queries to 
+     * variable nQueries. Consequently stream gives two numbers to be read
+     * which stored to map nResponses and finally queries' tokens which are
+     * stores to variable queriesTokens according to queries' ids
      * @param stream is the stream to be read
      */
     void readQueriesFile(ifstream& stream);
 private:
-	//input file stream for the documents 
+    //input file stream for the documents 
     ifstream									documentsText;
-	//input file stream for the queries
+    //input file stream for the queries
     ifstream									queriesText;
-	//number of documents
-	//TODO: nDocuments and nQueries to be a 
-	//variable map<bool, size_t> nDocs so that 
-	//the bool part to answer the query isQuery
-	size_t										nDocuments;
-	//number of queries
-	size_t										nQueries;
-	//a list for each document containing its 
-	//terms
-	//TODO: replace pointer with a vector
+    //number of documents
+    //TODO: nDocuments and nQueries to be a 
+    //variable map<bool, size_t> nDocs so that 
+    //the bool part to answer the query isQuery
+    size_t									nDocuments;
+    //number of queries
+    size_t									nQueries;
+    //a list for each document containing its 
+    //terms
+    //TODO: replace pointer with a vector
     list<string>*								documentsTokens;
-	//a list for each query containing its terms
-	//TODO: replace pointer with a vector
+    //a list for each query containing its terms
+    //TODO: replace pointer with a vector
     list<string>*								queriesTokens;
     //key: the query id, value: the number of 
-	//responses that will be returned for this 
-	//query
-	map <size_t, size_t>						nResponses;
+    //responses that will be returned for this 
+    //query
+    map <size_t, size_t>							nResponses;
     
     /**
      * it transforms a string to its lower case
      * @param s the string to be transformed
      * @return the trasnformed string
      */
-    string makeLower(const string& s);
+     string makeLower(const string& s);
     
     /**
      * it removes the punctuation from a string 
@@ -140,14 +140,14 @@ private:
      * @param punct the string of punctuation's characters
      * @return the string without punctuation
      */
-    string removePunct(const string& s, const string& punct);
+     string removePunct(const string& s, const string& punct);
     
     /**
      * it uses the functions makeLower and removePunct
      * @param s the string to be transformed
      * @return a string without punctuation and in a lower case
      */
-    string lowerRemovedPunct(const string& s);
+     string lowerRemovedPunct(const string& s);
 };
 
 #endif /* PROCESSFILES_H */
