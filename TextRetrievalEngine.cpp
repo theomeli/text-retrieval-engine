39bb2d8819661d16b10295070d6b570d977dc3e1
Theodosis Melidis <melidisteo@Gmail.com>
2017-07-18 17:16:11 +0300

    Update TextRetrievalEngine.cpp

--- TextRetrievalEngine.cpp
+++ TextRetrievalEngine.cpp
@@ -37,31 +37,31 @@ TextRetrievalEngine::TextRetrievalEngine() {
 
 
 TextRetrievalEngine::TextRetrievalEngine(ProcessFiles* p) {
-	this -> p = new ProcessFiles;
+    this -> p = new ProcessFiles;
     *(this->p) = *p;
     
-	//initializing frequencies
+    //initializing frequencies
     vector<map<string, size_t>> temp1(p->getNQueries() + 1, map<string, size_t>());
-	frequencies[true] = temp1;
-	vector<map<string, size_t>> temp2(p->getNDocuments() + 1, map<string, size_t>());
-	frequencies[false] = temp2;
+    frequencies[true] = temp1;
+    vector<map<string, size_t>> temp2(p->getNDocuments() + 1, map<string, size_t>());
+    frequencies[false] = temp2;
 
-	//initializing weights
-	vector<vector<double>> temp3(p->getNQueries() + 1, vector<double>());
-	weights[true] = temp3;
-	vector<vector<double>> temp4(p->getNDocuments() + 1, vector<double>());
-	weights[false] = temp4;
+    //initializing weights
+    vector<vector<double>> temp3(p->getNQueries() + 1, vector<double>());
+    weights[true] = temp3;
+    vector<vector<double>> temp4(p->getNDocuments() + 1, vector<double>());
+    weights[false] = temp4;
 
-	//initializing maxFrequencies
-	vector<size_t> temp5(p->getNQueries() + 1, 1);
-	maxFrequencies[true] = temp5;
-	vector<size_t> temp6(p->getNDocuments() + 1, 1);
-	maxFrequencies[false] = temp6;
+    //initializing maxFrequencies
+    vector<size_t> temp5(p->getNQueries() + 1, 1);
+    maxFrequencies[true] = temp5;
+    vector<size_t> temp6(p->getNDocuments() + 1, 1);
+    maxFrequencies[false] = temp6;
 }
 
 
 TextRetrievalEngine::TextRetrievalEngine(const TextRetrievalEngine& orig)
-	:frequencies(orig.getFrequencies()), maxFrequencies(orig.getMaxFrequencies()) {
+    :frequencies(orig.getFrequencies()), maxFrequencies(orig.getMaxFrequencies()) {
     *(this->p) = *(orig.getP());
 }
 
@@ -73,15 +73,15 @@ TextRetrievalEngine::~TextRetrievalEngine() {
 
 size_t TextRetrievalEngine::computeFreqOfTerm(const string& term, size_t docId, bool isTermOfQuery) {
     list<string>::iterator iter;
-	size_t frequency = 0;
+    size_t frequency = 0;
 
-	//update frequency for a document term
+    //update frequency for a document term
     if (!isTermOfQuery) {
         for (iter = p->getDocumentsTokens()[docId].begin(); iter != p->getDocumentsTokens()[docId].end(); iter++)
             if (*iter == term)
                 frequency++;
-	}
-	//update frequency for a query term
+    }
+    //update frequency for a query term
     else {
         for (iter = p->getQueriesTokens()[docId].begin(); iter != p->getQueriesTokens()[docId].end(); iter++)
             if (*iter == term)
@@ -93,47 +93,47 @@ size_t TextRetrievalEngine::computeFreqOfTerm(const string& term, size_t docId, 
 
 
 void TextRetrievalEngine::computeFrequencies() {
-	//initialize frequencies and insert terms of documents
-	size_t nDocuments = p->getNDocuments();
+    //initialize frequencies and insert terms of documents
+    size_t nDocuments = p->getNDocuments();
     list<string>::iterator iter;
     for (size_t i = 0; i <= nDocuments; i++) 
         for (iter = p->getDocumentsTokens()[i].begin(); iter != p->getDocumentsTokens()[i].end(); iter++) {
             frequencies[false][i][*iter] = 0;
-			terms.insert(*iter);
+	    terms.insert(*iter);
         }
     
-	//initialize frequencies and insert terms of queries
+    //initialize frequencies and insert terms of queries
     size_t nQueries = p->getNQueries();
     for (size_t i = 0; i <= nQueries; i++) {
         for (iter = p->getQueriesTokens()[i].begin(); iter != p->getQueriesTokens()[i].end(); iter++) {
             terms.insert(*iter);
-			frequencies[true][i][*iter] = 0;
-		}
+	    frequencies[true][i][*iter] = 0;
 	}
+    }
     
     for (size_t i = 0; i <= nDocuments; i++)
         for (iter = p->getDocumentsTokens()[i].begin(); iter != p->getDocumentsTokens()[i].end(); iter++)
             frequencies[false][i][*iter]++;
     
-	for (size_t i = 0; i <= nQueries; i++)
+    for (size_t i = 0; i <= nQueries; i++)
         for (iter = p->getQueriesTokens()[i].begin(); iter != p->getQueriesTokens()[i].end(); iter++)
-			frequencies[true][i][*iter]++;
+	    frequencies[true][i][*iter]++;
 }
 
 
 void TextRetrievalEngine::computeMaxFreq(size_t docId, bool isQuery) {
-	map<string, size_t> myMap =  frequencies[isQuery][docId];
+    map<string, size_t> myMap =  frequencies[isQuery][docId];
     
     for (auto const &ent1 : myMap)
-		maxFrequencies[isQuery][docId] = max(ent1.second, maxFrequencies[isQuery][docId]);
+	maxFrequencies[isQuery][docId] = max(ent1.second, maxFrequencies[isQuery][docId]);
 }
 
 
 size_t TextRetrievalEngine::getNDocsWithTerm(const string term) {
-	size_t nDocsWithTerm = 0;
-	size_t nDocuments = p->getNDocuments();
+    size_t nDocsWithTerm = 0;
+    size_t nDocuments = p->getNDocuments();
     for (size_t i = 0; i <= nDocuments; i++) {
-		if (frequencies[false][i].find(term) != frequencies[false][i].end()) {
+	if (frequencies[false][i].find(term) != frequencies[false][i].end()) {
             nDocsWithTerm++;
         }
     }
@@ -143,34 +143,34 @@ size_t TextRetrievalEngine::getNDocsWithTerm(const string term) {
 
 
 void TextRetrievalEngine::initializeIdfs() {
-	vector<double> temp(terms.size() + 1, 0);
-	idfs[true] = temp;
-	idfs[false] = temp;
+    vector<double> temp(terms.size() + 1, 0);
+    idfs[true] = temp;
+    idfs[false] = temp;
 }
 
 
 void TextRetrievalEngine::computeIdfs(bool isQuery) {
-	size_t nDocuments = p->getNDocuments();
-	size_t i = 1;
-	for (auto const &term : terms) {
-		double nt = getNDocsWithTerm(term);
-		//nt == 0 outputs division with zero error
-		if (nt == 0)
-			nt = 1;
-		double temp = log(double(nDocuments / nt));
-		if (isQuery == true)
-			idfs[isQuery][i] = temp;
-		else {
-			idfs[isQuery][i] = temp / log(nDocuments); 
-			temp = temp / log(nDocuments);
-		}
-		i++;
+    size_t nDocuments = p->getNDocuments();
+    size_t i = 1;
+    for (auto const &term : terms) {
+        double nt = getNDocsWithTerm(term);
+	//nt == 0 outputs division with zero error
+	if (nt == 0)
+	    nt = 1;
+	double temp = log(double(nDocuments / nt));
+	if (isQuery == true)
+	    idfs[isQuery][i] = temp;
+	else {
+	    idfs[isQuery][i] = temp / log(nDocuments); 
+	    temp = temp / log(nDocuments);
 	}
+        i++;
+    }
 }
 
 
 double TextRetrievalEngine::computeNormalizedFreq(const string term, const size_t documentId, bool isTermOfQuery) {
-	double result = computeFreqOfTerm(term, documentId, isTermOfQuery) / double(maxFrequencies[isTermOfQuery][documentId]);
+    double result = computeFreqOfTerm(term, documentId, isTermOfQuery) / double(maxFrequencies[isTermOfQuery][documentId]);
     if (!isTermOfQuery)
         return result;
     else
@@ -179,96 +179,96 @@ double TextRetrievalEngine::computeNormalizedFreq(const string term, const size_
 
 
 void TextRetrievalEngine::computeDocWeight(const size_t documentId, bool isQuery) {
-	size_t i = 1;
-	for (auto const &term : terms) {
-		weights[isQuery][documentId].push_back(computeNormalizedFreq(term, documentId, isQuery) * idfs[isQuery][i]);
-		i++;
-	}
+    size_t i = 1;
+    for (auto const &term : terms) {
+	weights[isQuery][documentId].push_back(computeNormalizedFreq(term, documentId, isQuery) * idfs[isQuery][i]);
+	i++;
+    }
 }
 
 
 void TextRetrievalEngine::computeDocsWeight(bool isQuery) {
-	computeIdfs(isQuery);
+    computeIdfs(isQuery);
 
-	for (size_t i = 1; i < weights[isQuery].size(); i++) {
-		computeMaxFreq(i, isQuery);
-		computeDocWeight(i, isQuery);
-	}
+    for (size_t i = 1; i < weights[isQuery].size(); i++) {
+	computeMaxFreq(i, isQuery);
+	computeDocWeight(i, isQuery);
+    }
 }
 
 
 double TextRetrievalEngine::computeLength(vector<double> aVector) {
-	double result = 0;
-	vector<double>::iterator iter;
-	for (iter = aVector.begin(); iter != aVector.end(); iter++)
-		result += pow(*iter, 2);
+    double result = 0;
+    vector<double>::iterator iter;
+    for (iter = aVector.begin(); iter != aVector.end(); iter++)
+	result += pow(*iter, 2);
 
-	return sqrt(result);
+    return sqrt(result);
 }
 
 
 double TextRetrievalEngine::computeCosine(vector<double> query, vector<double> document) {
-	double result = 0;
-	for (size_t i = 1; i < query.size(); i++) {
-		result += query[i] * document[i];
-	}
+    double result = 0;
+    for (size_t i = 1; i < query.size(); i++) {
+	result += query[i] * document[i];
+    }
 
-	return result / (computeLength(query) * computeLength(document));
+    return result / (computeLength(query) * computeLength(document));
 }
 
 
 priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> TextRetrievalEngine::getSortedSimilarities(size_t queryId, size_t nResponses) {
-	vector<double> temp(p->getNDocuments() + 1, 0);
+    vector<double> temp(p->getNDocuments() + 1, 0);
 
-	//it stores all the cosines between query with queryId and documents of the collection
-	for (size_t i = 1; i <= p->getNDocuments(); i++) {
-		temp[i] = computeCosine(weights[true][queryId], weights[false][i]);
-	}
+    //it stores all the cosines between query with queryId and documents of the collection
+    for (size_t i = 1; i <= p->getNDocuments(); i++) {
+	temp[i] = computeCosine(weights[true][queryId], weights[false][i]);
+    }
 
-	//it makes pairs of term id and its associated weight ordered by the weight
-	priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> temp1;
-	for (size_t i = 1; i < temp.size(); i++) {
-		pair<size_t, double> p = make_pair(i, temp[i]);
-		temp1.push(p);
-	}
+    //it makes pairs of term id and its associated weight ordered by the weight
+    priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> temp1;
+    for (size_t i = 1; i < temp.size(); i++) {
+	pair<size_t, double> p = make_pair(i, temp[i]);
+	temp1.push(p);
+    }
 
-	//it pushes nResponses pairs to a queue named result
-	priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> result;
-	for (size_t i = 0; i < nResponses; i++) {
-		result.push(temp1.top());
-		temp1.pop();
-	}
+    //it pushes nResponses pairs to a queue named result
+    priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> result;
+    for (size_t i = 0; i < nResponses; i++) {
+	result.push(temp1.top());
+	temp1.pop();
+    }
 
-	return result;
+    return result;
 }
 
 
 void TextRetrievalEngine::displayResults() {
-	for (size_t i = 1; i <= p->getNQueries(); i++) {
-		size_t nResponses = p->getNResponses()[i];
-		cout << "Query to search: " << endl;
-		list<string>::iterator iter;
-		//it displays the query with id i
-		for (iter = p->getQueriesTokens()[i].begin(); iter != p->getQueriesTokens()[i].end(); iter++)
-			cout << *iter << ' ';
-		cout << endl << "===================" << endl;
-		cout << "Returned documents:";
-		cout << endl << "===================" << endl;
-		priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> simularities = getSortedSimilarities(i, nResponses);
-		size_t size = simularities.size();
-		for (size_t k = 0; k < size; k++) {
-			size_t docSize = 0;
-			pair<size_t, double> aPair = simularities.top();
-			simularities.pop();
-			//it displays the documents resulted from the call of getSortedSimilarities
-			//also stores document's size and use it for a formatted display
-			for (iter = p->getDocumentsTokens()[aPair.first].begin(); iter != p->getDocumentsTokens()[aPair.first].end(); iter++) {
-				cout << *iter << ' ';
-				docSize += iter->size();
-				docSize++;
-			}
-			cout << setw(70 - docSize) << "(with weight " << aPair.second << ')' << endl; 
-		}
-		cout << endl;
-	}
+    for (size_t i = 1; i <= p->getNQueries(); i++) {
+	size_t nResponses = p->getNResponses()[i];
+	cout << "Query to search: " << endl;
+	list<string>::iterator iter;
+	//it displays the query with id i
+	for (iter = p->getQueriesTokens()[i].begin(); iter != p->getQueriesTokens()[i].end(); iter++)
+	    cout << *iter << ' ';
+	cout << endl << "===================" << endl;
+	cout << "Returned documents:";
+	cout << endl << "===================" << endl;
+	priority_queue<pair<size_t, double>, vector<pair<size_t, double>>, Compare> simularities = getSortedSimilarities(i, nResponses);
+	size_t size = simularities.size();
+	for (size_t k = 0; k < size; k++) {
+	    size_t docSize = 0;
+	    pair<size_t, double> aPair = simularities.top();
+	    simularities.pop();
+	    //it displays the documents resulted from the call of getSortedSimilarities
+	    //also stores document's size and use it for a formatted display
+	    for (iter = p->getDocumentsTokens()[aPair.first].begin(); iter != p->getDocumentsTokens()[aPair.first].end(); iter++) {
+		cout << *iter << ' ';
+		docSize += iter->size();
+		docSize++;
+	    }
+	    cout << setw(70 - docSize) << "(with weight " << aPair.second << ')' << endl; 
+        }
+        cout << endl;
+    }
 }
